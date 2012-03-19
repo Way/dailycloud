c452f2c07de63616c144d367d37728ffb4ae1ece
Alexander Vey <this.vey@gmail.com>
2012-03-19 13:34:01 +0100

    update sql structure

--- dailycloud.sql
+++ dailycloud.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 10, 2011 at 09:23 AM
+-- Generation Time: Mar 19, 2012 at 12:33 PM
 -- Server version: 5.1.53
 -- PHP Version: 5.3.4
 
@@ -22,24 +22,149 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `bookmarkmap`
+--
+
+CREATE TABLE IF NOT EXISTS `bookmarkmap` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `userid` int(10) unsigned NOT NULL,
+  `bookmarkid` int(10) unsigned NOT NULL,
+  `created` int(10) unsigned NOT NULL,
+  `deleted` enum('0','1') NOT NULL DEFAULT '0',
+  `new` enum('0','1') NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `userid` (`userid`,`bookmarkid`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `bookmarks`
 --
 
 CREATE TABLE IF NOT EXISTS `bookmarks` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user` int(11) NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `url` varchar(200) NOT NULL,
-  `title` varchar(50) NOT NULL,
-  `date` int(11) NOT NULL,
-  `snapshot` varchar(200) NOT NULL,
-  `category` text,
-  `tags` text,
+  `title` varchar(140) NOT NULL,
+  `webshot` varchar(200) NOT NULL,
+  `description` text NOT NULL,
+  `created` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `url` (`url`),
-  KEY `user` (`user`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  FULLTEXT KEY `description` (`description`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `bookmarks`
+-- Table structure for table `notes`
 --
 
+CREATE TABLE IF NOT EXISTS `notes` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `userid` int(10) unsigned NOT NULL,
+  `groupid` int(10) unsigned NOT NULL,
+  `text` text,
+  `created` int(10) unsigned NOT NULL,
+  `updated` int(10) unsigned NOT NULL,
+  `version` int(10) unsigned NOT NULL DEFAULT '0',
+  `deleted` enum('0','1') NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `userid` (`userid`,`groupid`),
+  KEY `created` (`created`,`updated`),
+  FULLTEXT KEY `text` (`text`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessions`
+--
+
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `id` varchar(40) NOT NULL,
+  `data` text,
+  `expires` int(16) unsigned NOT NULL,
+  `fingerprint` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings`
+--
+
+CREATE TABLE IF NOT EXISTS `settings` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `userid` int(10) unsigned NOT NULL,
+  `types` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `userid` (`userid`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tagmap`
+--
+
+CREATE TABLE IF NOT EXISTS `tagmap` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `userid` int(10) unsigned NOT NULL,
+  `tagid` int(10) unsigned NOT NULL,
+  `targetid` int(10) unsigned NOT NULL,
+  `target` enum('bookmarks','notes','dates','tags') NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `tagid` (`tagid`,`targetid`,`target`),
+  KEY `userid` (`userid`,`tagid`,`targetid`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+CREATE TABLE IF NOT EXISTS `tags` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `userid` int(10) unsigned NOT NULL,
+  `name` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `userid` (`userid`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userdetails`
+--
+
+CREATE TABLE IF NOT EXISTS `userdetails` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `profile_image` varchar(2000) DEFAULT NULL,
+  `status_text` text,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `oauth_provider` enum('facebook','twitter','google') DEFAULT NULL,
+  `oauth_uid` text,
+  `oauth_token` text,
+  `oauth_secret` text,
+  `username` varchar(32) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `token` varchar(36) DEFAULT NULL,
+  `secret` text,
+  `created` int(10) unsigned DEFAULT NULL,
+  `last_login` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
